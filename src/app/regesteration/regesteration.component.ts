6172a92bbf13caf2707418a0a6c3fb2fb8ddaa08
athulprakash007 <athull362@gmail.com>
2022-10-03 21:31:28 -0700

    finished

---
+++ src/app/regesteration/regesteration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regesteration',
+  templateUrl: './regesteration.component.html',
+  styleUrls: ['./regesteration.component.css']
+})
+export class RegesterationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
